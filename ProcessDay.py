7807f465d4e85ab2adf882acffe40f074a13909a
F4lc0nCt <F4lc0nCt@github.com>
2021-12-07 22:00:02 +0100

    [DAY-X] Deduce current Day with filename

--- ProcessDay.py
+++ ProcessDay.py
@@ -1,20 +1,30 @@
 
+import importlib
 from DayFactory import DayFactory
 
-NB_DAY = 7
+
+NB_MAX_DAY = 25
 LAST_DAY = True
 SECOND_STAR = True
 
+""" Get the number of Day implemented"""
+for d in range(1, NB_MAX_DAY):
+    day_name = "Day%d" % d
+    try:
+        getattr(importlib.import_module(day_name), day_name)
+        nb_day = d
+    except ModuleNotFoundError:
+        break
 
-dayFactory = DayFactory(NB_DAY)
+dayFactory = DayFactory(nb_day)
 if LAST_DAY:
-    day = dayFactory.get_day(NB_DAY)
+    day = dayFactory.get_day(nb_day)
     day.process_first_star()
     if SECOND_STAR:
-        day = dayFactory.get_day(NB_DAY)
+        day = dayFactory.get_day(nb_day)
         day.process_second_star()
 else:
-    for i in range(1, NB_DAY+1):
+    for i in range(1, nb_day+1):
         day = dayFactory.get_day(i)
         day.process_first_star()
         if SECOND_STAR:
